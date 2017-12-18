b867d5b7e8a8ea473c9eff20386f0850f0d88c65
Uriel <ur13l.infante@gmail.com>
2017-12-18 16:18:22 -0600

    Cambios

--- public/js/jornadas/partidos.js
+++ public/js/jornadas/partidos.js
@@ -8,12 +8,14 @@
      * Funcionalidad de botón para agregar partido.
      */
     $("#agregar_partido").click(function(e) {
-        var div = $("<div/>");
-        div.addClass('div_partido');
+        var div = $("<div/>").addClass('row').addClass('partido_editar');
+        var id = guid();
+        div.attr('id', id);
         div.append($("#partido_base").html());
         $("#table_partidos").append(div);
         $("#empty_partidos").hide();
+        $(`#${id} .select_equipo`).chosen();
     });
 
-   $(".select_equipo").selectric();
  });
+
---
+++ public/js/utils/guid.js
@@ -0,0 +1,13 @@
+
+ /**
+  * Metodo para generar un valor aleatorio que se usará como id en algunos elementos.
+  */
+  function guid() {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+      s4() + '-' + s4() + s4() + s4();
+  }
