485754833babee62e706f42236afa2052877172d
Uriel <ur13l.infante@gmail.com>
2018-08-20 16:08:32 -0500

    Changes

--- public/js/quinielas/info.js
+++ public/js/quinielas/info.js
@@ -1,4 +1,4 @@
-$(function() {
+$(document).ready(function() {
 
     $(".item-jornada").click(function(){ 
         var id_jornada = $(this).data('id'),
@@ -17,6 +17,7 @@ $(function() {
             },
             success:function (data) {
                 $("#datos_jornada").html(data);
+                table();
             }
         });
 
@@ -31,5 +32,52 @@ $(function() {
                 $("#resultados_jornada").html(data);
             }
         });
+
+
     });
+    table();
 });
+
+
+function table() {
+
+    $('#example').DataTable({
+        scrollY:        "220px",
+        scrollX:        true,
+        scrollCollapse: true,
+        fixedColumns:   {
+            leftColumns: 2
+        },
+        oLanguage: {
+            "sProcessing":     "Procesando...",
+            "sLengthMenu": 'Mostrar <select>'+
+                '<option value="10">10</option>'+
+                '<option value="20">20</option>'+
+                '<option value="30">30</option>'+
+                '<option value="40">40</option>'+
+                '<option value="50">50</option>'+
+                '<option value="-1">All</option>'+
+                '</select> registros',
+            "sZeroRecords":    "No se encontraron resultados",
+            "sEmptyTable":     "Ningún dato disponible en esta tabla",
+            "sInfo":           "Mostrando del (_START_ al _END_) de un total de _TOTAL_ registros",
+            "sInfoEmpty":      "Mostrando del 0 al 0 de un total de 0 registros",
+            "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+            "sInfoPostFix":    "",
+            "sSearch":         "Filtrar:",
+            "sUrl":            "",
+            "sInfoThousands":  ",",
+            "sLoadingRecords": "Por favor espere - cargando...",
+            "oPaginate": {
+                "sFirst":    "Primero",
+                "sLast":     "Último",
+                "sNext":     "Siguiente",
+                "sPrevious": "Anterior"
+            },
+            "oAria": {
+                "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+                "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+            }
+        }
+    });
+}
